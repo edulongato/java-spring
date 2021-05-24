e25fa59ddce48c527ed8e3e075a02c5c5a5986c0
Eduardo Longato <edulongato@yahoo.com.br>
2021-05-24 20:26:06 -0300

    Initial commit

---
+++ src/main/java/br/com/longato/javaspringideexample/JavaSpringIdeExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.longato.javaspringideexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringIdeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringIdeExampleApplication.class, args);
+	}
+
+}
